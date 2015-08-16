4e1f1b81a5124d9769a5ca233b2f7b2c08e2d649
Will Smith <smithywill@gmail.com>
2015-08-16 20:03:40 +0100

    Static files

--- byteblok/settings.py
+++ byteblok/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 
 # Parse database configuration from $DATABASE_URL
