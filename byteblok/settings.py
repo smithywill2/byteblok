bbe17fb7e2ad21204463de938e4342d360c5b4c9
Will Smith <smithywill@gmail.com>
2015-08-16 19:58:23 +0100

    Changed where static files are looked up from

--- byteblok/settings.py
+++ byteblok/settings.py
@@ -116,6 +116,4 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../bytebloksite/static'),)
