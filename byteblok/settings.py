f811cca15ae5a0ff125d0757fd57712af90c1da2
Will Smith <smithywill@gmail.com>
2015-08-16 18:40:35 +0100

    Changing static locatio

--- byteblok/settings.py
+++ byteblok/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
 # Parse database configuration from $DATABASE_URL
